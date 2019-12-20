11c59a0bcf9364e20260712b8b87c328cf8bdb3c
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-12-20 00:53:16 -0500

    update

--- yelpfe/src/components/svg/svg.js
+++ yelpfe/src/components/svg/svg.js
@@ -9,12 +9,12 @@ function Svg(props) {
             <>
             {console.log("SVG props check",props.data)}
             <div>
-                <svg width="100%" height="100%" class="circle-chart" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
-                    <circle class="circle-chart-background" stroke="#efefef" stroke-width="3" fill="none" cx="21" cy="21" r="15.91549431"></circle>
-                    <circle class="circle-chart-circle" stroke="hsl(0, 72%, 48%)" stroke-width="3" stroke-dasharray={`100,100`} stroke-linecap="round" fill="none" cx="21" cy="21" r="15.91549431"></circle>
-                    <g class="gpa-text">
-                        <text x="50%" y="50%" class="chart-label">{props.value}</text>
-                        <text x="50%" y="50%" class="chart-number">{props.data}</text>
+                <svg width="100%" height="100%" className="circle-chart" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
+                    <circle className="circle-chart-background" stroke="#efefef" strokeWidth="3" fill="none" cx="21" cy="21" r="15.91549431"></circle>
+                    <circle className="circle-chart-circle" stroke="hsl(0, 72%, 48%)" strokeWidth="3" strokeDasharray={`100,100`} strokeLinecap="round" fill="none" cx="21" cy="21" r="15.91549431"></circle>
+                    <g className="gpa-text">
+                        <text x="50%" y="50%" className="chart-label">{props.value}</text>
+                        <text x="50%" y="50%" className="chart-number">{props.data}</text>
                     </g>
                 </svg>
             </div>
@@ -25,19 +25,18 @@ function Svg(props) {
         <>
         {/* {let count = props.data/props.total*100)} */}
         <div>
-            <svg width="100%" height="100%" class="circle-chart" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
-                <circle class="circle-chart-background" stroke="#efefef" stroke-width="3" fill="none" cx="21" cy="21" r="15.91549431"></circle>
-                <circle class="circle-chart-circle" stroke="hsl(0, 72%, 48%)" stroke-width="3" stroke-dasharray={`${count},100`} stroke-linecap="round" fill="none" cx="21" cy="21" r="15.91549431"></circle>
-                <g class="gpa-text">
-                    <text x="50%" y="50%" class="chart-label">{props.value}</text>
-                    <text x="50%" y="50%" class="chart-number">{props.data}</text>
+            <svg width="100%" height="100%" className="circle-chart" viewBox="0 0 42 42" xmlns="http://www.w3.org/2000/svg">
+                <circle className="circle-chart-background" stroke="#efefef" strokeWidth="3" fill="none" cx="21" cy="21" r="15.91549431"></circle>
+                <circle className="circle-chart-circle" stroke="hsl(0, 72%, 48%)" strokeWidth="3" strokeDasharray={`${count},100`} strokeLinecap="round" fill="none" cx="21" cy="21" r="15.91549431"></circle>
+                <g className="gpa-text">
+                    <text x="50%" y="50%" className="chart-label">{props.value}</text>
+                    <text x="50%" y="50%" className="chart-number">{props.data}</text>
                 </g>
             </svg>
         </div>
         </>
-    ); 
+    );
     }
-    
 }
 
 export default Svg
